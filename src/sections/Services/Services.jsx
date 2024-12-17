5f7a7f6682be307b2f2b13109be51751a1e8f5ac
Tamara Vdb <vdb.tamara@gmail.com>
2024-12-17 11:02:43 +0100

    Add Services section

--- src/sections/Services/Services.jsx
+++ src/sections/Services/Services.jsx
@@ -1,9 +1,79 @@
+import { BsArrowDownRight } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
+const services = [
+   {
+      num: "01",
+      title: "Web Development",
+      description: "Développement d'applications web interactives et de sites sur mesure.",
+      href: "https://www.zestcitron.be/#services",
+   },
+   {
+      num: "02",
+      title: "UI/UX Design",
+      description: "Analyse des besoins et conception centrée sur l’utilisateur final.",
+      href: "https://www.zestcitron.be/#services",
+   },
+   {
+      num: "03",
+      title: "Web Design",
+      description: "Conception de sites optimisés performance et accessibilité.",
+      href: "https://www.zestcitron.be/#services",
+   },
+   {
+      num: "04",
+      title: "SEO",
+      description: "Chaque site est optimisé au référencement naturel.",
+      href: "https://www.zestcitron.be/#services",
+   },
+];
+
 const Services = () => {
    return (
-      <>
-         <h2>Services</h2>
-      </>
+      <div className="container mx-auto">
+         <div className="flex flex-col justify-center py-8 xl:py-16">
+            <motion.div
+               initial={{ opacity: 0 }}
+               animate={{
+                  opacity: 1,
+                  transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+               }}
+               className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
+            >
+               {services.map((service, index) => {
+                  return (
+                     <div
+                        key={index}
+                        className="flex-1 flex flex-col justify-center gap-6 group"
+                     >
+                        {/* top */}
+                        <div className="w-full flex justify-between items-center">
+                           <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">
+                              {service.num}
+                           </div>
+                           <Link
+                              to={service.href}
+                              className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45"
+                           >
+                              <BsArrowDownRight className="text-primary text-3xl" />
+                           </Link>
+                        </div>
+                        {/* title */}
+                        <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">
+                           {service.title}
+                        </h2>
+                        {/* description */}
+                        <p className="text-white/60">{service.description}</p>
+                        {/* border */}
+                        <div className="border-b border-white/20 w-full"></div>
+                     </div>
+                  );
+               })}
+            </motion.div>
+         </div>
+      </div>
    );
 };
 
-export default Services;
+export default Services;;
