91f5cc93581dc75ed49aa93ac5c7b0f573cceeb0
Tamara Vdb <vdb.tamara@gmail.com>
2024-12-20 11:35:19 +0100

    Contact section

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -1,8 +1,96 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+
+const info = [
+   {
+      icon: <FaPhoneAlt />,
+      title: "Téléphone",
+      description: "(+32) 477 73 26 49",
+   },
+   {
+      icon: <FaEnvelope />,
+      title: "Email",
+      description: "youremail@gmail.com",
+   },
+   {
+      icon: <FaMapMarkerAlt />,
+      title: "Localisation",
+      description: "Bruxelles",
+   },
+];
+
+import { motion } from "framer-motion";
+
 const Contact = () => {
    return (
-      <>
-         <h2>Contact</h2>
-      </>
+      <div className="container mx-auto">
+         <div className="flex flex-col justify-center py-8 xl:py-16">
+            <h2 className="mx-auto lg:mx-0 mb-10 text-lg bg-white/80 text-primary w-fit rounded py-1 px-3">Contact</h2>
+            <motion.section
+               initial={{ opacity: 0 }}
+               animate={{
+                  opacity: 1,
+                  transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+               }}
+            >
+               <div className="flex flex-col xl:flex-row gap-[30px]">
+
+                  {/* Form */}
+                  <div className="xl:w-[54%]">
+                     <form className="flex flex-col gap-6 p-10 bg-[#23262e] rounded-xl">
+                        <h3 className="text-4xl text-accent">Travaillons ensemble !</h3>
+                        <p className="text-white/90">
+                           Une idée, une question ou juste envie de dire bonjour ?
+                           Laissez-moi un petit mot, je serais ravie de vous lire.
+                        </p>
+
+                        {/* Input */}
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                           <Input type="firstname" placeholder="Prénom" />
+                           <Input type="lastname" placeholder="Nom" />
+                           <Input type="email" placeholder="Email" />
+                           <Input type="phone" placeholder="Téléphone" />
+                        </div>
+
+                        {/* Textarea */}
+                        <Textarea
+                           className="h-[200px]"
+                           placeholder="Message"
+                        />
+
+                        {/* Btn */}
+                        <Button size="md" className="max-w-40">
+                           Envoyer
+                        </Button>
+
+                     </form>
+                  </div>
+
+                  {/* Info */}
+                  <div className="flex-1 flex items-center xl:justify-end mb-8 xl:mb-0">
+                     <ul className="flex flex-row xl:flex-col gap-6">
+                        {info.map((item, index) => {
+                           return (
+                              <li key={index} className="flex items-center gap-6">
+                                 <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#23262e] text-accent rounded-md flex items-center justify-center">
+                                    <div className="text-[28px]">{item.icon}</div>
+                                 </div>
+                                 <div className="flex-1">
+                                    <p className="text-white/70">{item.title}</p>
+                                    <h3 className="text-xl font-extralight">{item.description}</h3>
+                                 </div>
+                              </li>
+                           );
+                        })}
+                     </ul>
+                  </div>
+               </div>
+            </motion.section>
+         </div>
+      </div>
    );
 };
 
