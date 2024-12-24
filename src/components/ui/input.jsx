6ac84b9240be3fa2b5eaa218c7564641bd827f2f
Tamara <vdb.tamara@gmail.com>
2024-12-24 14:18:16 +0100

    Input 100%

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -7,7 +7,7 @@ const Input = React.forwardRef(({ className, type, ...props }, ref) => {
       <input
          type={type}
          className={cn(
-            "flex h-10 rounded-md border border-white/10 focus:border-accent font-extralight bg-primary px-4 py-4 text-base placeholder:text-white/60 outline-none",
+            "flex h-10 rounded-md border border-white/10 focus:border-accent font-extralight bg-primary px-4 py-4 text-base placeholder:text-white/60 outline-none w-full",
             className
          )}
          ref={ref}
