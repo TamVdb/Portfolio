d061276481982344bb3ff4c1355787bd6e1de774
Tamara Vdb <vdb.tamara@gmail.com>
2025-01-06 14:04:22 +0100

    Add className to follow clicks on GA4

--- src/components/Social/Social.jsx
+++ src/components/Social/Social.jsx
@@ -17,7 +17,7 @@ const Social = () => {
                   href={item.path}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="w-10 h-10 border border-accent rounded-full flex justify-center items-center text-accent text-xl hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                  className="w-10 h-10 border border-accent rounded-full flex justify-center items-center text-accent text-xl hover:bg-accent hover:text-primary hover:transition-all duration-500 linkSocials"
                >
                   {item.icon}
                </a>
