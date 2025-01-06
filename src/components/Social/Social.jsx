7641a0a3c575d40659d93691de9f214dccfb94f4
Tamara Vdb <vdb.tamara@gmail.com>
2025-01-06 14:16:38 +0100

    Add text to icon link for GA4

--- src/components/Social/Social.jsx
+++ src/components/Social/Social.jsx
@@ -2,9 +2,11 @@ import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import { BsGlobe } from "react-icons/bs";
 
 const socials = [
-   { icon: <FaGithub />, path: "https://github.com/TamVdb" },
-   { icon: <FaLinkedinIn />, path: "https://www.linkedin.com/in/tamaravandebroeck/" },
-   { icon: <BsGlobe />, path: "https://www.zestcitron.be/" },
+   { icon: <FaGithub />, path: "https://github.com/TamVdb", name: "GitHub" },
+   {
+      icon: <FaLinkedinIn />, path: "https://www.linkedin.com/in/tamaravandebroeck/", name: "LinkedIn"
+   },
+   { icon: <BsGlobe />, path: "https://www.zestcitron.be/", name: "ZestCitron" },
 ];
 
 const Social = () => {
@@ -20,6 +22,7 @@ const Social = () => {
                   className="w-10 h-10 border border-accent rounded-full flex justify-center items-center text-accent text-xl hover:bg-accent hover:text-primary hover:transition-all duration-500 linkSocials"
                >
                   {item.icon}
+                  <span className="sr-only">[{item.name}]</span>
                </a>
             );
          })}
